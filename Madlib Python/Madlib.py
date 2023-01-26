4a4994fe5d5588689477573ee111c201cdb8df91
LazyCiao <123471880+LazyCiao@users.noreply.github.com>
2023-01-26 14:58:41 +0100

    Add files via upload

---
+++ Madlib Python/Madlib.py
@@ -0,0 +1,31 @@
+adj1 = input('Adjective: ')
+adj2 = input('Adjective: ')
+name1 = input('Name: ')
+noun1 = input('Noun: ')
+noun2 = input('Noun: ')
+adj3 = input('Adjective: ')
+noun3 = input('Noun: ')
+adj4 = input('Adjective: ')
+noun4 = input('Noun: ')
+adj5 = input('Adjective: ')
+noun5 = input('Noun: ')
+noun6 = input('Noun: ')
+adj6 = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+verb3 = input('Verb: ')
+adverb = input('Adverb: ')
+
+Madlib = f"Once upon a time, in a {adj1} land far, far away, there lived a {adj2} prince named {name1}. \
+He was the ruler of the {noun1} kingdom and was loved by all of his {noun2} subjects. \
+One day, the prince heard about a {adj3} princess who was trapped in a {noun3} tower by a {adj4} dragon. \
+Without hesitation, the prince set out on his {noun5} horse to rescue her. \
+Along the way, he met a {adj5} wizard who gave him a {noun6} potion to help him defeat the dragon. \
+The prince also encountered a {adj5} troll who tried to {verb1} him, \
+but he was able to {verb2} the troll and continue on his journey. \
+Finally, the prince reached the tower and defeated the dragon with the help of the potion. \
+He saved the princess and they fell in {verb3} in love. They rode off into the sunset and lived {adj6} ever after."
+
+print(Madlib)
+
+# Python Beginner Project -- Madlib
